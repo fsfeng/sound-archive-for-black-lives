f10e7a737f5d4251eb256c6defb91eb26c07fc0e
FFeng <fsfeng@users.noreply.github.com>
2021-01-25 23:31:41 -0500

    edits

--- src/components/ArchiveList-dev.js
+++ src/components/ArchiveList-dev.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Head from "next/head";
 import { Box, Text } from "@chakra-ui/react";
+import { createClient } from "contentful";
+// import * as contentful from "contentful";
 
-const client = require("contentful").createClient({
-  space: "jm1v8d9gwzzo",
-  accessToken: "BfPwj4m630OxWB9SKM_CX5jKJuPpMnqbxOvYSxB2O5o"
+// const client = require("contentful").createClient({
+//   space: "jm1v8d9gwzzo",
+//   accessToken: "BfPwj4m630OxWB9SKM_CX5jKJuPpMnqbxOvYSxB2O5o"
+// });
+
+// const client = require("contentful").createClient({
+//   space: "cuaq03l2zpaa",
+//   accessToken: "m2e_oubaIZOedUJIU_VrRFxd7jp1l9fgAqRLYLB2xwU"
+// });
+
+const client = createClient({
+  space: "cuaq03l2zpaa",
+  accessToken: "m2e_oubaIZOedUJIU_VrRFxd7jp1l9fgAqRLYLB2xwU"
 });
 
 // export async function getStaticProps() {
@@ -19,7 +31,7 @@ const client = require("contentful").createClient({
 // }
 
 export const getStaticProps = async () => {
-  const data = await client.getEntries();
+  const data = await client.getEntries().then((response) => response.items);
   return {
     props: {
       archiveRecordings: data.items
